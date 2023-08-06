bcb55d5a585394d9841502a608f5846b6fccbfd7
joshuie <jaitonit@yahoo.co.uk>
2023-08-06 02:43:45 +0100

    remove meal workout

--- js/app.js
+++ js/app.js
@@ -29,6 +29,33 @@ class CalorieTracker {
         this._render();
     }
 
+    removeMeal(id) {
+        const index = this._meals.findIndex((meal) => meal.id === id);
+        if (index !== -1) {
+            const meal = this._meals[index];
+            this._meals.splice(index, 1);
+            this._totalCalories -= meal.calories;
+            this._render();
+        }
+    }
+
+    removeWorkout(id) {
+        const index = this._workouts.findIndex((workout) => workout.id === id);
+        if (index !== -1) {
+            const workout = this._workouts[index];
+            this._workouts.splice(index, 1);
+            this._totalCalories += workout.calories;
+            this._render();
+        }
+    }
+
+    reset() {
+        this._totalCalories = 0;
+        this._meals = [];
+        this._workouts = [];
+        this._render();
+    }
+
     //Private Methods
 
     _displayCaloriesTotal() {
@@ -170,6 +197,18 @@ class App{
         document
             .getElementById('workout-form')
             .addEventListener('submit', this._newItem.bind(this, 'workout'));
+
+        document.getElementById('meal-items')
+            .addEventListener('click', this._removeItem.bind(this, 'meal'));
+        
+        document.getElementById('filter-meals')
+            .addEventListener('keyup', this._filterItems.bind(this, 'meal'));
+        
+        document.getElementById('filter-workouts')
+            .addEventListener('keyup', this._filterItems.bind(this, 'workout'));
+        
+        document.getElementById('reset')
+            .addEventListener('click', this._reset.bind(this));
     }
 
     _newItem(type, e) {
@@ -179,15 +218,16 @@ class App{
         const calories = document.getElementById(`${type}-calories`);
 
         //Validate inputs
-        if (name.value === '' || calories.value === '') { 
-            alert('Please fill in the fields');
+        if (name.value === '' || calories.value === '') {
+            alert('Please fill in all fields');
             return;
         }
 
-        if (type === 'meal') {            
+        if (type === 'meal') {
             const meal = new Meal(name.value, +calories.value);
             this._tracker.addMeal(meal);
-        } else {
+        }
+        if (type === 'workout') {
             const workout = new Workout(name.value, +calories.value);
             this._tracker.addWorkout(workout);
         }
@@ -197,8 +237,43 @@ class App{
 
         const collapseItem = document.getElementById(`collapse-${type}`);
         const bsCollapse = new bootstrap.Collapse(collapseItem, {
-            toggle: true
+            toggle: true, 
         });
+    }
+
+    _removeItem(type, e) {
+        if (e.target.classList.contains('delete') || e.target.classList.contains('fa-xmark')) {
+            if (confirm('Are you sure?')) {
+                const id = e.target.closest('.card').getAttribute('data-id');
+                
+                type === 'meal'
+                    ? this._tracker.removeMeal(id)
+                    : this._tracker.removeWorkout(id);
+                
+                e.target.closeset('.card').remove();
+            }
+        }
+    }
+
+    _filterItems(type, e) {
+        const text = e.target.value.toLowerCase();
+        document.querySelectorAll(`#${type}-items .card`).forEach(item => {
+            const name = item.firstElementChild.firstElementChild.textContent;
+
+            if (name.toLowerCase().indexOf(text) !== -1) {
+                item.style.display = 'block';
+            } else {
+                item.style.display = 'none';
+            }
+        });
+    }
+
+    _reset() {
+        this._tracker.reset();
+        document.getElementById('meal-items').innerHTML = '';
+        document.getElementById('workout-items').innerHTML = '';
+        document.getElementById('filter-meals').value = '';
+        document.getElementById('filter-workouts').value = '';
     }
 }
 
